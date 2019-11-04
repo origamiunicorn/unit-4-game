ab2d12f78c064973696d8a9665e4d388cbef5b4c
origami unicorn <rvwright@gmail.com>
2019-11-03 21:40:48 -0800

    Moved the changes pushed to visible HP counters and the damage listings to the Fight button. Added a game win conditional.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
     var charAttack;
     var enemyCounter;
     var attackNum = 0;
+    var enemyName;
 
     // Hide the reset button
     $("#playItAgain").hide();
@@ -23,9 +24,8 @@ $(document).ready(function () {
             charHP = parseInt($("#theChosenOne").children("div").attr("hp"));
             charAttack = parseInt($("#theChosenOne").children("div").attr("ap"));
 
-            // Find all divs left in #atTheBeginning...
+            // Find all divs left in #atTheBeginning, add enemyFighters class, set background to red, and move to #beMyEnemy div.
             var remainingCharacters = $("#atTheBeginning").find("div");
-            // ... add enemyFighters class, set background to red, and move to #beMyEnemy div.
             remainingCharacters.addClass("enemyFighters").css("background-color", "red").detach().appendTo("#beMyEnemy");
 
             charaSelect++; // Increase character selection by one increment
@@ -44,9 +44,10 @@ $(document).ready(function () {
                     return;
                 }
 
-                $(this).addClass("activeEnemy").css("background-color", "green").detach().appendTo("#aFleshWound");
+                $(this).removeClass("enemyFighters").addClass("activeEnemy").css("background-color", "green").detach().appendTo("#aFleshWound");
                 enemyHP = parseInt($("#aFleshWound").children("div").attr("hp"));
                 enemyCounter = parseInt($("#aFleshWound").children("div").attr("cap"));
+                enemyName = $("#aFleshWound").find(".characterName").text();
 
             });
         });
@@ -55,45 +56,61 @@ $(document).ready(function () {
     $("#overNineThousand").click(function (e) {
         e.preventDefault();
 
-        // Allow each value (enemy or char HP) to be subtracted from on click; with attackNum increasing each click, the charAttack will increase with each subsequent fight.
-        enemyHP -= (charAttack + (charAttack * attackNum));
-        console.log(enemyHP);
-        charHP -= enemyCounter;
-        console.log(charHP);
+        // make a variable that checks if #aFleshWound has an enemy in it. If not, then return and do nothing.
+        var isEnemyThere = $("#aFleshWound").children("div");
+        console.log(isEnemyThere);
 
-        attackNum++;
-
-        lookingForWin();
-
-    });
-
-    function lookingForWin() {
-        // If neither character nor enemy have zero (or less) HP left, take their current HP, subtract the enemy attack value, and then update text to page. But am not updating right now their scores, look at this and what needs to happen. Happens once! 
-
-        if (charHP > 0 && enemyHP > 0) {
+        if (isEnemyThere.length > 0) {
+            // Allow each value (enemy or char HP) to be subtracted from on click; with attackNum increasing each click, the charAttack will increase with each subsequent fight.
+            enemyHP -= (charAttack + (charAttack * attackNum));
+            console.log(enemyHP);
+            charHP -= enemyCounter;
+            console.log(charHP);
 
             // Update the visible HP on the character cards
             $("#theChosenOne").find(".healthPoints").text(charHP);
             $("#aFleshWound").find(".healthPoints").text(enemyHP);
 
-            // State on page what happened.
-            $("#theWord").html("<span class='playerDamage'>You attacked " + $("#aFleshWound").find(".characterName").text() + " for " + (charAttack + (charAttack * attackNum)) + " damage. </span> <br />");
-            $("#theWord").append("<span class='enemyDamage'> " + $("#aFleshWound").find(".characterName").text() + " attacked you back for " + enemyCounter + " damage. </span> <br />");
+            // Announce the damage done to both parties.
+            $("#theWord").html("<span class='playerDamage'>You attacked " + enemyName + " for " + (charAttack + (charAttack * attackNum)) + " damage. </span> <br />");
+            $("#theWord").append("<span class='enemyDamage'> " + enemyName + " attacked you back for " + enemyCounter + " damage. </span> <br />");
 
-        } else if (enemyHP <= 0) {
+            lookingForWin();
+
+        } else {
+            alert("There's no one to attack!");
+            console.log(isEnemyThere);
+            return;
+        }
+
+        attackNum++;
+
+    });
+
+    function lookingForWin() {
+        // If neither character nor enemy have zero (or less) HP left, take their current HP, subtract the enemy attack value, and then update text to page. But am not updating right now their scores, look at this and what needs to happen. Happens once! 
+        var leftStanding = $("#beMyEnemy").children("div");
+        console.log(leftStanding);
+
+        if (charHP > 0 && enemyHP < 0 && leftStanding.length <= 0) {
+
+            // You won!
+            $("#theWord").html("<span>You have defeated all comers! Congratulations! You've won!</span>");
+            // And display a "Restart" button that on click reloads the page.
+            $("#playItAgain").show().click(function () {
+                location.reload(true);
+            });
+
+        } else if (charHP > 0 && enemyHP < 0 && leftStanding.length > 0) {
 
             // State in #theWord that you won, and detach the enemy characterCard. State "select another enemy".
-            // Okay, instead, make a hidden div to place all defeated enemies after defeat.
 
-            var enemyName;
-            enemyName = $("#aFleshWound").find(".characterName").text();
-
-            $("#theWord").html("<span>You have defeated " + enemyName + ", you can select another enemy to fight.</span>")
+            $("#theWord").html("<span>You have defeated " + enemyName + "! Select another enemy to fight.</span>");
             $(".activeEnemy").detach().appendTo("#graveyard").hide();
 
 
-        } else {
-            $("#theWord").html("<span>You have been defeated... GAME OVER!</span>")
+        } else if (charHP <= 0 && enemyHP > 0 || charHP <= 0 && enemyHP >= 0) {
+            $("#theWord").html("<span>You have been defeated... GAME OVER!</span>");
 
             // And display a "Restart" button that on click reloads the page.
             $("#playItAgain").show().click(function () {
